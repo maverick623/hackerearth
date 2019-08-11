df195b63736e27ce1cdc781e3498077fb145bc9c
Niladri Dutta Gupta <46416832+maverick623@users.noreply.github.com>
2019-08-11 15:15:23 +0530

    Update and rename New Text Document.py to Jadoo vs Koba.py

---
+++ Jadoo vs Koba.py
@@ -0,0 +1,19 @@
+'''Jadoo, the space alien has challenged Koba to a friendly duel.
+He asks Koba to write a program to print out all numbers from 70 to 80. 
+Knowing perfectly well how easy the problem is, the Jadoo adds his own twist to the challenge.
+He asks Koba to write the program without using a single number in his program and also restricts the character limit to 100.'''
+
+a = ord('F')
+i= ord('G') - a
+b = ord('P')
+
+d = b-a
+z = d-d
+
+while d>=z:
+    print(a)
+    a = a + i
+    d = d - i
+
+
+
