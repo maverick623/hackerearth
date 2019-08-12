3998556e91144f6036fe9c2412384568431c3faa
Niladri Dutta Gupta <46416832+maverick623@users.noreply.github.com>
2019-08-12 11:15:29 +0530

    Update Jadoo vs Koba.py
    ASCII Values can be used in range

--- Jadoo vs Koba.py
+++ Jadoo vs Koba.py
@@ -3,17 +3,8 @@ He asks Koba to write a program to print out all numbers from 70 to 80.
 Knowing perfectly well how easy the problem is, the Jadoo adds his own twist to the challenge.
 He asks Koba to write the program without using a single number in his program and also restricts the character limit to 100.'''
 
-a = ord('F')
-i= ord('G') - a
-b = ord('P')
-
-d = b-a
-z = d-d
-
-while d>=z:
-    print(a)
-    a = a + i
-    d = d - i
+for num in range(ord('F'),ord('Q')):
+    print(num)
 
 
 
